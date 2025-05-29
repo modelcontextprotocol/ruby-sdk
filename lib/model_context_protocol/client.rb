d1e7a83f71be4645d15dbf739d3084b0b60dad9e
Joey Cardosi <joey.cardosi@shopify.com>
2025-05-29 12:19:47 -0400

    add more robust error handling

--- lib/model_context_protocol/client.rb
+++ lib/model_context_protocol/client.rb
@@ -7,5 +7,16 @@
 module ModelContextProtocol
   module Client
     # Can be made an abstract class if we need shared behavior
+
+    class RequestHandlerError < StandardError
+      attr_reader :error_type, :original_error, :request
+
+      def initialize(message, request, error_type: :internal_error, original_error: nil)
+        super(message)
+        @request = request
+        @error_type = error_type
+        @original_error = original_error
+      end
+    end
   end
 end
